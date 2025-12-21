1286babef716fb52c44febdece11dcb0835ef9c9
Graham Hall <graham@ghall.dev>
2025-12-21 12:54:44 -0500

    basic validation for preference values

--- Sources/Otto/Commands/Set.swift
+++ Sources/Otto/Commands/Set.swift
@@ -5,13 +5,42 @@ func setCmd(domain: String?, key: String?, value: String?) throws {
     let (domainId, preferenceKey, valueType, resetWhenApplied) = try getPreferenceValues(
         prefs: prefs, domain: domain, key: key)
 
-    let cmd = "write \(domainId) \"\(preferenceKey)\" \(valueType.rawValue) \"\(value!)\""
+    let checkedValue = try checkValue(value, valueType: valueType)
 
-    let out = shell.defaults(cmd)
+    let cmd = "write \(domainId) \"\(preferenceKey)\" \(valueType.rawValue) \(checkedValue)"
 
-    print(out)
+    let _ = shell.defaults(cmd)
 
     if resetWhenApplied == true {
         restartDomain(domainId)
     }
+}
+
+func checkValue(_ value: String?, valueType: ValueType) throws -> String {
+    if value == nil {
+        throw OttoError.runtimeError("No value provided.")
+    }
+
+    let BOOL_VALUES = ["yes", "no", "true", "false"]
+
+    switch valueType {
+    case .bool:
+        if !BOOL_VALUES.contains(value!) {
+            throw OttoError.runtimeError("Invalid boolean value.")
+        }
+    case .int:
+        if Int(value!) == nil {
+            throw OttoError.runtimeError("Invalid integer value.")
+        }
+    case .float:
+        if Float(value!) == nil {
+            throw OttoError.runtimeError("Invalid float value.")
+        }
+    case .string:
+        return "\"\(value!)\""
+    default:
+        throw OttoError.runtimeError("Unhandled value type.")
+    }
+
+    return value!
 }
