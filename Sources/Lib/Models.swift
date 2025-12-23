0fe43df4cdcffd14a8b64d2796f58bf6bbf206c3
Graham Hall <graham@ghall.dev>
2025-12-22 21:24:15 -0500

    add more preferences

--- Sources/Lib/Models.swift
+++ Sources/Lib/Models.swift
@@ -14,12 +14,14 @@ struct Domain: Codable {
 
 struct Preference: Codable {
     var name: String
+    var desc: String
     var key: String
     var type: ValueType
     var options: [String]?
 
-    init(name: String, key: String, type: ValueType, options: [String]?) {
+    init(name: String, desc: String, key: String, type: ValueType, options: [String]?) {
         self.name = name
+        self.desc = desc
         self.key = key
         self.type = type
         self.options = options
